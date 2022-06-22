9e19ade94c518e46ebcb7914d2c5e14cf368044c
Dr Steven Transmisia <DrTransmisia@protonmail.com>
2022-06-22 15:22:27 +0200

    eps32

--- mistletoe/main.js
+++ mistletoe/main.js
@@ -3,12 +3,12 @@ let pyodide;
 async function main() {
 	pyodide = await loadPyodide();
 	await pyodide.loadPackage("micropip");
-	
+
 	console.log(pyodide.runPython(`
 		print("BOOOOOTING")
 		import micropip
 		micropip.install("mistletoe")
-		micropip.install("https://DrTransmisia.github.io/mistletoe/bussy.whl")
+		micropip.install("https://DrTransmisia.github.io/mistletoe/rdramamistletoe-0.0.1-py2.py3-none-any.whl")
 
 		import rdramamistletoe
 	`));
