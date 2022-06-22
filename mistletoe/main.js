aef6c79f9f7e7dd5d9f3f89683005f9cf5f2771a
Dr Steven Transmisia <DrTransmisia@protonmail.com>
2022-06-22 15:29:06 +0200

    fsdf

--- mistletoe/main.js
+++ mistletoe/main.js
@@ -4,14 +4,21 @@ async function main() {
 	pyodide = await loadPyodide();
 	await pyodide.loadPackage("micropip");
 
-	console.log(pyodide.runPython(`
+	pyodide.runPython(`
 		print("BOOOOOTING")
 		import micropip
 		micropip.install("mistletoe")
 		micropip.install("https://DrTransmisia.github.io/mistletoe/rdramamistletoe-0.0.1-py2.py3-none-any.whl")
 
-		import rdramamistletoe
-	`));
+		from rdramamistletoe.dramarender import DramaHTMLRenderer
+		from mistletoe import Document
+
+		def markdown(x):
+			r = DramaHTMLRenderer()
+			return r.render(Document(x))
+		
+		pass
+	`);
 };
 
 main();
